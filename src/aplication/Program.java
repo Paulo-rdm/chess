cfee823b5a5f289819373c41a0caac131b480859
Paulo-rdm <paulo.martins0127@gmail.com>
2024-01-09 15:15:27 -0300

    ajustes

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,6 +2,6 @@ package aplication;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Bom dia!");
+        System.out.println("Bom dia!!");
     }
 }
